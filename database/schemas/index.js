cdc32bb082d0e23ffe240ea4ab195128ea9556bc
dracudev <andreujavier99@gmail.com>
2025-08-01 15:45:52 +0200

    refactor: remove unnecessary comments

--- database/schemas/index.js
+++ database/schemas/index.js
@@ -4,15 +4,12 @@ const serviceSchema = require("./serviceSchema");
 const projectItemSchema = require("./projectItemSchema");
 const paymentSchema = require("./paymentSchema");
 
-
 module.exports = {
   Client: clientSchema,
   Project: projectSchema,
   Service: serviceSchema,
   ProjectItem: projectItemSchema,
   Payment: paymentSchema,
-
-  // Helper function to get all schemas
 
   getAll() {
     return {
@@ -24,7 +21,6 @@ module.exports = {
     };
   },
 
-  // Helper function to get schema by name
   getSchema(name) {
     return this[name];
   },
