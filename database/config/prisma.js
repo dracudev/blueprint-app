b15f8b4e87e0ddb357855bb61dce88f1a738f2aa
dracudev <andreujavier99@gmail.com>
2025-08-04 19:25:47 +0200

    Simplify Prisma client initialization to prevent constructor conflicts

--- database/config/prisma.js
+++ database/config/prisma.js
@@ -1,11 +1,14 @@
 const { PrismaClient } = require("@prisma/client");
 const PrismaModelFactory = require("../factories/PrismaModelFactory");
-const { initializePrismaForServerless } = require("../utils/serverless");
 
-const prisma = initializePrismaForServerless(PrismaClient, {
-  log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-  errorFormat: "pretty",
-});
+global.prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+    errorFormat: "pretty",
+  });
+
+const prisma = global.prisma;
 
 const modelFactory = new PrismaModelFactory(prisma);
 
